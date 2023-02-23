7d94ff6ba4081bf574c61b401bc3bb86f834554c
Bruno Clementino <bruno.clementino@miracledevs.com>
2023-02-23 17:03:28 -0300

    NavBar: Remove unused code

--- src/theme/Navbar/ExtendedNavbar/index.tsx
+++ src/theme/Navbar/ExtendedNavbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./ExtendedNavbar.module.scss";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
