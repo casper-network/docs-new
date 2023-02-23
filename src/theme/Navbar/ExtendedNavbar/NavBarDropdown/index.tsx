496a993f582a371de664dd751497cefdbfa71e80
Bruno Clementino <bruno.clementino@miracledevs.com>
2023-02-23 17:03:02 -0300

    Remove unused code

--- src/theme/Navbar/ExtendedNavbar/NavBarDropdown/index.tsx
+++ src/theme/Navbar/ExtendedNavbar/NavBarDropdown/index.tsx
@@ -1,5 +1,5 @@
 import INavItem from "src/plugins/docusaurus-plugin-navdata/src/interfaces/navbar/navItem";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import NavBarColumn from "./NavBarColumn";
 import styles from "./NavBarDropdown.module.scss";
 
@@ -8,19 +8,6 @@ interface INavBarDropdownProps {
     locale: string;
 }
 export default function NavBarDropdown({ content, locale }: INavBarDropdownProps) {
-    console.log(content);
-    /* useEffect(() => {
-        document.body.classList.add("dropdownOpen");
-        return () => {
-            document.body.classList.remove("dropdownOpen");
-            document.body.classList.add("dropdownIsClosing");
-
-            setTimeout(() => {
-                document.body.classList.remove("dropdownIsClosing");
-            }, 200);
-        };
-    }, []); */
-
     return (
         <div className={styles.wrapper} onClick={(e) => e.stopPropagation()}>
             <div className={styles.dropdown}>
