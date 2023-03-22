dcb921d646e2e31652bc4d0f9332fd27f9e943fb
Bruno Clementino <bruno.clementino@miracledevs.com>
2023-03-22 16:45:57 -0300

    Close SiteNavbar func from links

--- src/theme/Navbar/ExtendedNavbar/index.tsx
+++ src/theme/Navbar/ExtendedNavbar/index.tsx
@@ -186,6 +186,7 @@ export default function ExtendedNavbar() {
                     current={current}
                     siteConfig={siteConfig}
                     socialMedia={data.socialMedia}
+                    closeNavBarHandler={closeNavBarHandler}
                 />
             </div>
         </div>
